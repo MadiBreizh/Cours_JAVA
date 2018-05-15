ca8da5b7091fbde23ec067758fb456c8087f8f12
MadiBreizh <erwan@malangot.fr>
2018-05-15 09:20:44 +0000

    nouveau fichier : doc/allclasses-frame.html nouveau fichier : doc/allclasses-noframe.html nouveau fichier : doc/com/imie/Diagram_UML/Bad.html nouveau fichier : doc/com/imie/Diagram_UML/class-use/Bad.html nouveau fichier : doc/com/imie/Diagram_UML/package-frame.html nouveau fichier : doc/com/imie/Diagram_UML/package-summary.html nouveau fichier : doc/com/imie/Diagram_UML/package-tree.html nouveau fichier : doc/com/imie/Diagram_UML/package-use.html nouveau fichier : doc/constant-values.html nouveau fichier : doc/deprecated-list.html nouveau fichier : doc/help-doc.html nouveau fichier : doc/index-files/index-1.html nouveau fichier : doc/index-files/index-2.html nouveau fichier : doc/index.html nouveau fichier : doc/overview-tree.html nouveau fichier : doc/package-list nouveau fichier : doc/script.js nouveau fichier : doc/stylesheet.css modifié :         src/com/imie/Diagram_UML/Bad.java

--- src/com/imie/Diagram_UML/Bad.java
+++ src/com/imie/Diagram_UML/Bad.java
@@ -2,6 +2,11 @@ package com.imie.Diagram_UML;
 
 public class Bad extends Human {
 
+	/**
+	 * 
+	 * @param lastname le prénom
+	 * @param firstname le nom
+	 */
 	public Bad(String lastname, String firstname) {
 		super(lastname, firstname);
 	}
